147bdd65f83373efb6524d8881101c691a9d0d50
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-07-05 23:19:23 +0100

    Added new testing map and sprite images, added layers and colliding layers, created player class

--- src/scenes/MapScene.ts
+++ src/scenes/MapScene.ts
@@ -1,30 +1,50 @@
-import mapJson from "../assets/json/2DTowerDefense.json";
-import Sprites from "../assets/images/2DSprites.png";
+import mapJson from "../assets/json/platformer.json";
+import Sprites from "../assets/images/main_lev_build_1.png";
+import Player from "../classes/Player";
+import playerImage from '../assets/images/player/move_sprite_1.png'
 
 export class MapScene extends Phaser.Scene {
   constructor() {
     super("MapScene");
   }
 
-  init() {}
-
   preload() {
     this.load.tilemapTiledJSON("map", mapJson);
     this.load.image("tiles", Sprites);
+    this.load.image('player', playerImage)
   }
 
   create() {
     // this.createNewGame();
     const map = this.make.tilemap({ key: "map" });
-    const tileset = map.addTilesetImage("2Dsprites", "tiles", 32, 32);
-    const layer1 = map.createLayer(0, tileset);
-    layer1.setInteractive();
+    const tileset = map.addTilesetImage("main_lev_build_1", "tiles", 16, 16);
+
+    const layers = this.createLayers(map)
+    layers.platformsColliders.setCollisionByExclusion([-1], true)
+
+    this.player = this.createPlayer()
+    this.physics.add.collider(this.player, layers.platformsColliders);
+    this.cursors = this.input.keyboard.createCursorKeys();
+
   }
+
+  createLayers(map: Phaser.Tilemaps.Tilemap) {
+    const tileset = map.getTileset('main_lev_build_1');
+    const platformsColliders = map.createLayer('platforms_colliders', tileset!)!;
+    const environment = map.createLayer('environment', tileset!)
+    const platforms = map.createLayer('platforms', tileset!);
+    platformsColliders.setCollisionByProperty({collides: true});
+    return { environment, platforms, platformsColliders };
+  }
+
+  createPlayer() {
+    const player = new Player(this, 100, 250);
+    player.body.setGravityY(500);
+    player.setCollideWorldBounds(true);
+    return player;
+  
+  }
+  
 
   update() {}
-
-  private createNewGame() {
-    // this launches the game scene
-    this.scene.launch("game");
-  }
 }
